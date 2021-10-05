8a2498283b3cc33c7729c62eb6b8c24e046e9d4d
PabloHnunes <pablohenriquenunes@hotmail.com>
2021-10-05 11:20:38 -0300

    adiciona nomes aos menus dropdown

--- src/components/DropDownMenu/index.js
+++ src/components/DropDownMenu/index.js
@@ -7,6 +7,8 @@ import { ReactComponent as Tinder } from '../../assets/icons/tinder.svg';
 import { ReactComponent as Arrow1 } from '../../assets/icons/arrow-1.svg';
 import { ReactComponent as Arrow2 } from '../../assets/icons/arrow-2.svg';
 import { ReactComponent as Arrow3 } from '../../assets/icons/arrow-3.svg';
+import { ReactComponent as Perfil } from '../../assets/icons/perfil-1.svg';
+
 
 
 import { AreaDropMenu } from '../DropDownMenu/styled';
@@ -14,17 +16,17 @@ import { AreaDropMenu } from '../DropDownMenu/styled';
 function DropDrownMenu(){
     return(
         <AreaDropMenu>
-            <DropDrownItem href="#">
+            <DropDrownItem leftIcon={<Perfil />} href="#">
                 Meu Perfil
             </DropDrownItem>
             <DropDrownItem leftIcon={<TikTok />} rigth={<Arrow1 />}>
-
+                TikTok
             </DropDrownItem>
             <DropDrownItem leftIcon={<XBox />} rigth={<Arrow2 />}>
-
+                X-box
             </DropDrownItem>
             <DropDrownItem leftIcon={<Tinder />} rigth={<Arrow3 />}>
-
+                Tinder
             </DropDrownItem>
         </AreaDropMenu>
     );
